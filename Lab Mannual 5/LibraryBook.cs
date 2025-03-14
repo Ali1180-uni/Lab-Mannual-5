370f0dd1fb8f04ae0f3ecd3ab8d21cc0372996a0
Ali1180-uni <24ntucsfl1180@student.ntu.edu.pk>
2025-03-14 10:07:16 +0500

    copy constructor added

--- Lab Mannual 5/LibraryBook.cs
+++ Lab Mannual 5/LibraryBook.cs
@@ -18,6 +18,11 @@ namespace Lab_Mannual_5
             this.author = author;
             this.isAvaliabe = isAvaliabe;
         }
-
+        public LibraryBook(LibraryBook x1)
+        {
+            this.title = x1.title;
+            this.author = x1.author;
+            this.isAvaliabe = x1.isAvaliabe;
+        }
     }
 }
